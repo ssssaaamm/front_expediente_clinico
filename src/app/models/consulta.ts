cf1e78238c304f5b6a5bfc6a9763661f5a2c8a36
Fredy Kcrez <zholary@gmail.com>
2017-05-14 21:35:03 -0600

    CRUD de consulta con error en el modal

--- src/app/models/consulta.ts
+++ src/app/models/consulta.ts
@@ -1,15 +1,18 @@
+﻿import { Especialidad } from './especialidad';
+
 export class Consulta{
     
     constructor(
         public codigo:string, 
         public nombre:string,
         public costo:number,
+        public especialidad:Especialidad,
         //?=opcional
         public id?:number 
     ){}
 
 // Esnecesario. algo de refeencias!!!
     public clone():Consulta{
-        return new Consulta(this.codigo,this.nombre,this.costo,this.id);
+        return new Consulta(this.codigo,this.nombre,this.costo,this.especialidad.clone(),this.id);
     }
 }
