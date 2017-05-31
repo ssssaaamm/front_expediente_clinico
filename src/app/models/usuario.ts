3b80e6a0db5f65e924836e65d205620988b3c58f
lara <rstorrento.25@gmail.com>
2017-05-31 00:09:19 -0600

    models usuario

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -2,19 +2,10 @@ import {Rol} from "app/models/rol";
 export class Usuario{
     constructor(
         public id:number    , 
-        public nombre1: string,
-        public nombre2: string,
-        public apellido1: string,
-        public apellido2: string,
-        public apellido_casada: string,
-        public pais: string,
-        public division: string,
-        public subdivision:string,
-        public tel_fijo: string,
-        public tel_movil: string,
-        public email: string,
         public username: string,
         public password: string,
+        public estado: boolean,
+        public last_login: Date, 
         public rol: Rol
 
         
@@ -22,10 +13,10 @@ export class Usuario{
 
 // Esnecesario. algo de refencias!!!
     public clone():Usuario{
-        return new Usuario(this.id,this.nombre1,this.nombre2,this.apellido1,this.apellido2,this.apellido_casada,this.pais,this.division,this.subdivision,this.tel_fijo,this.tel_movil,this.email,this.username,this.password,this.rol);
+        return new Usuario(this.id,this.username,this.password,this.estado,this.last_login,this.rol);
     }
 
     public full_clone():Usuario{
-        return new Usuario(this.id,this.nombre1,this.nombre2,this.apellido1,this.apellido2,this.apellido_casada,this.pais,this.division,this.subdivision,this.tel_fijo,this.tel_movil,this.email,this.username,this.password,this.rol);
+        return new Usuario(this.id,this.username,this.password,this.estado,this.last_login,this.rol).clone();
     }
 }
