c8185db06fb478b2b1193224166aa052b774fcde
lara <rstorrento.25@gmail.com>
2017-06-16 15:52:12 -0600

    Vistas cirugia-consultas de paciente

---
+++ src/app/layout/expediente-paciente/cirugias/mod-detalle/mod-detalle.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'app-mod-detalle',
+  templateUrl: './mod-detalle.component.html',
+  styleUrls: ['./mod-detalle.component.scss']
+})
+export class ModDetalleComponent implements OnInit {
+
+  public exito: boolean;
+  public mensaje: string;
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) { }
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+      this.clearFields();
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      this.clearFields();
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+
+  ngOnInit() {
+  }
+
+  private clearFields() {
+
+  }
+
+
+}
