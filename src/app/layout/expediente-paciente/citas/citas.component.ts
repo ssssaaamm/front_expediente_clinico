f2f000d74077b6c19a3ecd4b380bcce2ccdce82a
enriqu3 <debian@localhost.localdomain>
2017-06-19 01:51:26 -0600

    citas

--- src/app/layout/expediente-paciente/citas/citas.component.ts
+++ src/app/layout/expediente-paciente/citas/citas.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+import { CitasService } from "app/services/citas.service"
 
 @Component({
   selector: 'app-citas',
   templateUrl: './citas.component.html',
-  styleUrls: ['./citas.component.scss']
+  styleUrls: ['./citas.component.scss'],
+  providers: [CitasService]
 })
 export class CitasComponent implements OnInit {
 
-  constructor() { }
+  public asignacion_citas: Array<any>;
+  public exito: boolean;
+  public mensaje: string;
+  closeResult: string;
+
+
+  constructor(private modalService: NgbModal, private citasService: CitasService) { }
 
   ngOnInit() {
+
+     let identidad = JSON.parse(localStorage.getItem("identity"));
+
+    console.log("ENVIANDO IDENTIDAD: " + JSON.stringify(identidad));
+     this.citasService.detail1(identidad).subscribe(
+            response=>{
+                console.log(response);
+               this.asignacion_citas = response;
+            },
+            error=>{
+                if(error!=null) {
+                    console.log("Error al enviar la peticion: "+error);
+                }
+            }
+        );
   }
 
 }
--- src/app/layout/expediente-paciente/citas/mod-add/mod-add.component.ts
+++ src/app/layout/expediente-paciente/citas/mod-add/mod-add.component.ts
@@ -1,30 +1,36 @@
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
-
+import { CitasService } from "app/services/citas.service";
+import { Especialidad } from "app/models/especialidad";
+import { Medico } from 'app/models/medico';
+import { Consulta } from 'app/models/consulta';
+import { ConsultasService } from 'app/services/consultas.service';
 @Component({
   selector: 'app-mod-add',
   templateUrl: './mod-add.component.html',
   styleUrls: ['./mod-add.component.scss'],
-  providers: [NgbModal]
+  providers: [CitasService,ConsultasService]
 })
 export class ModAddComponent implements OnInit {
 
-  public fecha_cita:any;
-  public hora_cita:any = {hour: 13, minute: 30};
+  //public fecha_cita:any;
+ // public hora_cita:any = {hour: 13, minute: 30};
 
   public exito: boolean;
   public mensaje: string;
+  public consultas:  Array<any> = new Array<any>();
+  public cita: any={};
   closeResult: string;
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private citasService: CitasService, private consultasService: ConsultasService) { }
 
   open(content) {
     this.modalService.open(content).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
-      this.clearFields();
+   //   this.clearFields();
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-      this.clearFields();
+   //   this.clearFields();
     });
   }
 
@@ -39,16 +45,42 @@ export class ModAddComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.obtenerServicioConsulta();
   }
 
+  obtenerServicioConsulta(){
+    this.consultasService.listServices()
+    .map((consultas: Array<any>)=>{
+      return consultas;
+    })
+    .subscribe( res => this.consultas = res);
 
-
+  }
+ 
   onSubmit() {
+/*
+            this.citasService.add(this.cita).subscribe(
+            response=>{
+                console.log(response);
+                if(response.status == "exito"){
+                    this.examen.id=response.id;
+                    this.examenes.push(this.examen.clone());
+                    this.exito=true;
+                    this.mensaje=response.mensaje;
+                }else{
+                    this.exito=false;
+                    this.mensaje=response.mensaje;
+                }
+            },
+            error=>{
+                if(error!=null) {
+                    console.log("Error al enviar la peticion: "+error);
+                }
+            }
+        );
+
+  }*/
+
 
   }
-
-  private clearFields() {
-
-  }
-
 }
